bea3ff5cf556e5ff89d3c8f5dee983dd4e7e3a45
dipyadav <dipti.yadav@publicissapient.com>
2023-05-03 00:36:05 +0530

    week 4 assignment

--- week3/ThreadObject.java
+++ week3/ThreadObject.java
@@ -11,54 +11,31 @@ class ThreadGenerator implements Runnable {
     }
 
     @Override
-
     public void run() {
-
         while (i < 21) {
-
             synchronized (lock) {
-
                 while (i % 2 != rem) {
-
                     try {
-
                         lock.wait();
-
                     } catch (InterruptedException e) {
-
                         e.printStackTrace();
-
                     }
                 }
-
                 System.out.println(Thread.currentThread().getName() + " " + i);
-
                 i++;
-
                 lock.notifyAll();
-
             }
-
         }
-
     }
 }
 
 public class ThreadObject {
-
     public static void main(String args[]) {
-
         ThreadGenerator evenThread = new ThreadGenerator(0);
-
         ThreadGenerator oddThread = new ThreadGenerator(1);
-
         Thread thread1 = new Thread(evenThread);
-
         Thread thread2 = new Thread(oddThread);
-
         thread1.start();
-
         thread2.start();
-
     }
 }
